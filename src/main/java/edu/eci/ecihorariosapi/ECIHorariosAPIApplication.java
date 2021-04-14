33f3f82049f0cd3c354abdd99c4addac6eea026e
David Coronado <david.coronado@escuelaing.edu.co>
2021-04-14 09:40:27 -0500

    change

--- src/main/java/edu/eci/ecihorariosapi/ECIHorariosAPIApplication.java
+++ src/main/java/edu/eci/ecihorariosapi/ECIHorariosAPIApplication.java
@@ -5,6 +5,7 @@
  */
 package edu.eci.ecihorariosapi;
 
+import java.util.Collections;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,13 +14,20 @@ import org.springframework.context.annotation.ComponentScan;
  *
  * @author David Coronado
  */
-
 @SpringBootApplication
 @ComponentScan(basePackages = {"edu.eci.ecihorarios"})
 public class ECIHorariosAPIApplication {
-    
-	public static void main(String[] args) {
-		SpringApplication.run(ECIHorariosAPIApplication.class, args);
-	}
-    
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ECIHorariosAPIApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port",getPort()));
+        app.run(args);
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 8080; //returns default port if heroku-port isn't set (i.e. on localhost)
+    }
 }
