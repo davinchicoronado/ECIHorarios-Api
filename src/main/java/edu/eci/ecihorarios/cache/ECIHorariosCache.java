933a0197eff842dbb49ad4fe80c73caadcb12fac
David Coronado <david.coronado@escuelaing.edu.co>
2021-05-24 18:46:35 -0500

    refactorCacheandaddDiagramClass

--- src/main/java/edu/eci/ecihorarios/cache/ECIHorariosCache.java
+++ src/main/java/edu/eci/ecihorarios/cache/ECIHorariosCache.java
@@ -4,20 +4,21 @@
  * and open the template in the editor.
  */
 package edu.eci.ecihorarios.cache;
-;
+
 
 import edu.eci.ecihorarios.model.bean.Curriculum;
 import edu.eci.ecihorarios.model.bean.Subject;
-import edu.eci.ecihorarios.model.bean.User;
 import edu.eci.ecihorarios.persistence.PersistenceException;
 
 /**
  *
  * @author David Coronado
  */ 
-public interface ECIHorariosCache {
+public interface ECIHorariosCache { 
+    
     
     public Curriculum getCurriculum(String carrer) throws PersistenceException;
+     
     
     public Subject getSubject(String code) throws PersistenceException; 
     
